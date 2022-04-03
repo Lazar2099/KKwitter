b1314304184606fcd574720210d259c18fe727f4
Lazar2099 <74484199+Lazar2099@users.noreply.github.com>
2022-04-03 10:15:54 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,12 @@
-function addUser(){
-    user_name=document.getElementById("user_name").value;
-    localStorage.setItem("user_name",user_name);
-    window.location="kwitter_room.html";
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyBh_F30r6KNSNTN1uK6_JzZ7ezjLZ6kpMw",
+    authDomain: "kwitter-2-38a8d.firebaseapp.com",
+    databaseURL: "https://kwitter-2-38a8d-default-rtdb.firebaseio.com",
+    projectId: "kwitter-2-38a8d",
+    storageBucket: "kwitter-2-38a8d.appspot.com",
+    messagingSenderId: "442159989796",
+    appId: "1:442159989796:web:2226a18c5348ff1e1544b1"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
